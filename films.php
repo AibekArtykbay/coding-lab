df59ea5740774e6572c762a89f5735499a5027be
AibekArtykbay <79966592+AibekArtykbay@users.noreply.github.com>
2021-05-31 18:28:35 +0600

    Add files via upload

---
+++ films.php
@@ -0,0 +1,68 @@
+<?php require_once 'db.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>films</title>
+  <link rel="stylesheet" href="MCS.css">
+    <script src="Final.js" defer></script> 
+	
+	<!-- Bootstrap -->
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	
+		<!-- Bootstrap -->
+</head>
+<body>
+<div class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+         
+        </button>
+        <a class="navbar-brand" href="#">Over<i class="fa fa-circle">View</i></a>
+      </div>
+      <div class="navbar-collapse collapse">
+        <ul class="nav navbar-nav navbar-right">
+          <li class="active"><a href="index.php">Home</a></li>
+          <li><a href="Series.html">Series</a></li>
+          <li><a href="Cartoons.html"> Cartoons</a></li>
+          <li><a href="Final_WEB.html">Contact</a></li>
+          <li><a href="#"><i class="fa fa-envelope-o" aria-hidden="true"></i></a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+ 
+
+  <!-- *****************************************************************************************    -->
+
+
+  <div id="overview">
+   <div id="faq">
+  <h2>Films</h2>
+ 
+    <div class="img">
+      <img src="https://www.film.ru/sites/default/files/styles/epsa_400x450/public/filefield_paths/40157723-1176341.jpg" alt="">
+      <span class = "n">«Тихое место 2»</span>
+    
+	
+	  <div class="img">
+      <img src="https://www.film.ru/sites/default/files/styles/epsa_400x450/public/filefield_paths/1563027536_0_0_817_460_1920x0_80_0_0_f9edc8b444d1fcca986949043a3b703f.jpg" alt="">
+      <span class = "n3"> «Гарри Поттера» </span>
+    </div>
+  </div>
+  </div>
+  
+  
+  
+  
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
